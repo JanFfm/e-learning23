63a312229d26e66021783ca69d714ce7fcb3a573
Tim Diedrich <s2197581@stud.uni-frankfurt.de>
2023-11-14 13:03:09 +0100

    listening and speaking exercice

--- project/app/views.py
+++ project/app/views.py
@@ -5,6 +5,10 @@ from django.core.cache import cache
 import random
 import datetime
 from .models import  Progress, Word
+from gtts import gTTS
+from io import BytesIO
+import speech_recognition as sr
+import pyttsx3
 
 
 @login_required
@@ -20,7 +24,7 @@ def homepage(request):
 @login_required
 def learn(request):
     if request.method == "GET":
-        learn_modes = [multiple_choice, word_translation]  # place other learn methods as function here:
+        learn_modes = [multiple_choice, word_translation, listening_comprehension, speaking_exercice]  # place other learn methods as function here:
         random_choice = learn_modes[random.randint(0, len(learn_modes) - 1 )]
         return random_choice(request)
         
@@ -34,6 +38,10 @@ def learn(request):
                 return eval_multiple_choice(request, word)
             case "word_translation":
                 return eval_word_translation(request, word)
+            case "listening_comprehension":
+                return eval_listening_comprehension(request, word)
+            case "speaking_exercice":
+                return eval_speaking_exercice(request, word)
             # and here for evaluation:
         
        
@@ -82,3 +90,60 @@ def eval_word_translation(request, word: Word):
     return eval_multiple_choice(request, word)
 
 
+
+
+def listening_comprehension(request):
+
+    word = Word.objects.all().order_by('?').first()
+    cache.set('mode', 'listening_comprehension', 30)
+    cache.set("word", word.pk)
+
+    question = word.word
+    tts = gTTS(question)
+    print("saving file..")
+    
+    #mp3_fp = BytesIO()
+    #tts.write_to_fp(mp3_fp)
+    tts.save('media/hello.mp3')
+
+    template = "app/listening_comprehension.html"
+    return render(request, template, context={"question": question})
+
+def eval_listening_comprehension(request, word: Word):
+    print(request.POST)
+    progress_obj, _ = Progress.objects.get_or_create(user=request.user, word=word)
+
+    answer = request.POST['answer']
+    if answer.lower()  == word.translation.lower():
+        messages.success(request, "Das war richtig.")
+        progress_obj.increase()
+    else:
+        messages.error(request, "Das war leider falsch!")
+        progress_obj.decrease()
+    return redirect("learn")
+
+
+def speaking_exercice(request):
+    word = Word.objects.all().order_by('?').first()
+    cache.set('mode', 'speaking_exercice', 30)
+    cache.set("word", word.pk)
+    
+    #question = word.word
+    question = word.translation
+    
+    template = "app/speaking_exercice.html"
+    return render(request, template,context={"question": question} )
+
+def eval_speaking_exercice(request, word: Word):
+    print(request.POST)
+    progress_obj, _ = Progress.objects.get_or_create(user=request.user, word=word)
+
+    answer = request.POST['ans']
+    print("This was the answer", answer)
+    if answer.lower()  == word.word.lower():
+        messages.success(request, "Das war richtig.")
+        progress_obj.increase()
+    else:
+        messages.error(request, "Das war leider falsch!")
+        progress_obj.decrease()
+    return redirect("learn")
--- project/project/settings.py
+++ project/project/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -133,7 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
