7b0b9fc546cbf956b6132eb2e3a0fc7d0497d62d
Tim Diedrich <s2197581@stud.uni-frankfurt.de>
2023-11-30 11:01:19 +0100

    added user lives

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('',  include('app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
